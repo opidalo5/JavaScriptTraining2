49497067537c401ecdc0813a8ba4644ea60fe3da
Opidalo5 <dipocoker@hotmail.com>
2021-03-03 15:37:23 +0100

    added logformulaire

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-// loading a variable into the DIV called "jsLoaded"
-var jsloaded=document.querySelector("#jsLoaded");
+function jsIsLoaded() {
+    // loading a variable into the DIV called "jsLoaded"
+    var jsLoaded = document.querySelector("#jsLoaded");
 
-// modification of the style css of the tag
-jsloaded.style.backgroundColor="green";
+    // modification of the style css of the tag
+    jsLoaded.style.backgroundColor = "green";
 
-// modification of the text content of the tag
-jsloaded.innerText="Le js est charge";
+    // modification of the text content of the tag
+    jsLoaded.innerText = "Le js est charge";
 
-console.log("Le fichier index.js a fini d'etre executer")
+    console.log("Le fichier index.js a fini d'etre executer");
+
+
+}
+jsIsLoaded();
+
+function logFormulaire() {
+    var formulaire = document.forms['my-form'];
+    console.log('title :', formulaire['form-title'].value);
+    console.log('author :', formulaire['form-author'].value);
+    console.log('hour :', formulaire['form-hour'].value);
+    console.log('date :', formulaire['form-date'].value);
+    console.log('mail :', formulaire['form-email'].value);
+    console.log('adresse :', formulaire['form-adresse'].value);
+    console.log('description :', formulaire['form-description'].value);
+
+}
