749091bd2db1360e8bcfe61a8c4b460f21deeda3
Opidalo5 <dipocoker@hotmail.com>
2021-03-03 10:42:10 +0100

    umm was this from yesterday?

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// loading a variable into the DIV called "jsLoaded"
+var jsloaded=document.querySelector("#jsLoaded");
+
+// modification of the style css of the tag
+jsloaded.style.backgroundColor="green";
+
+// modification of the text content of the tag
+jsloaded.innerText="Le js est charge";
+
+console.log("Le fichier index.js a fini d'etre executer")
